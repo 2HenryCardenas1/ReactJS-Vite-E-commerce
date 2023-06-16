f157e52553ef748a5d71693ee9b3ae94a680e857
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-06-16 16:28:20 -0500

    layout created

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import Layout from "../../components/Layout/Layout";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return <Layout>SignIn</Layout>;
 }
