ffacec65790c33ea21311826b61f1d9d740c72a8
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-06-16 16:28:41 -0500

    Card created, and api consumed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Card from "../../components/Card/Card";
+import Layout from "../../components/Layout/Layout";
 
 export default function Home() {
-  return <div className="bg-slate-500">Home</div>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  return (
+    <Layout>
+      Home
+      <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
